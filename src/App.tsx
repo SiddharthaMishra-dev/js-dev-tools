2d707e0a3ab4f40cffca7cb2a71bb44b90068a85
Siddhartha Mishra <mailbox.siddharthamishra@gmail.com>
2025-12-12 23:56:54 +0530

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { Analytics } from '@vercel/analytics/react';
 import { BrowserRouter, Routes, Route } from 'react-router';
 
 import Home from './routes/Home';
@@ -9,14 +10,17 @@ import ImageCropper from './routes/ImageCropper';
 
 export default function App() {
  return (
-  <BrowserRouter>
-   <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/image-to-base64" element={<ImageToBase64 />} />
-    <Route path="/image-format-converter" element={<ImageConverter />} />
-    <Route path="/image-compressor" element={<ImageCompressor />} />
-    <Route path="/image-cropper" element={<ImageCropper />} />
-   </Routes>
-  </BrowserRouter>
+  <>
+   <BrowserRouter>
+    <Routes>
+     <Route path="/" element={<Home />} />
+     <Route path="/image-to-base64" element={<ImageToBase64 />} />
+     <Route path="/image-format-converter" element={<ImageConverter />} />
+     <Route path="/image-compressor" element={<ImageCompressor />} />
+     <Route path="/image-cropper" element={<ImageCropper />} />
+    </Routes>
+   </BrowserRouter>
+   <Analytics />
+  </>
  );
 }
