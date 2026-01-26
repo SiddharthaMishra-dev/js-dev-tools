cf13b60c4a2b0cc199ef704553d599617d6eaed7
Siddhartha Mishra <mailbox.siddharthamishra@gmail.com>
2026-01-27 00:03:45 +0530

    chore: update vercel build

--- apps/web/.vercel/output/functions/__server.func/_ssr/index.mjs
+++ apps/web/.vercel/output/functions/__server.func/_ssr/index.mjs
@@ -3450,7 +3450,7 @@ function getResponse() {
   return event.res;
 }
 async function getStartManifest(matchedRoutes) {
-  const { tsrStartManifest } = await import("./_tanstack-start-manifest_v-DA11kwEw.mjs");
+  const { tsrStartManifest } = await import("./_tanstack-start-manifest_v-BEGf1_8n.mjs");
   const startManifest = tsrStartManifest();
   const rootRoute = startManifest.routes[rootRouteId] = startManifest.routes[rootRouteId] || {};
   rootRoute.assets = rootRoute.assets || [];
@@ -3922,7 +3922,7 @@ function getStartResponseHeaders(opts) {
 let entriesPromise;
 let manifestPromise;
 async function loadEntries() {
-  const routerEntry = await import("./router-Ce7LckN9.mjs");
+  const routerEntry = await import("./router-EYBqgctR.mjs");
   const startEntry = await import("./start-HYkvq4Ni.mjs");
   return { startEntry, routerEntry };
 }
---
+++ apps/web/.vercel/output/static/assets/IconBraces-CGHHPoEb.js
@@ -0,0 +1,1 @@
+import{c as a}from"./main-CGnZi2mW.js";const e=[["path",{d:"M7 4a2 2 0 0 0 -2 2v3a2 3 0 0 1 -2 3a2 3 0 0 1 2 3v3a2 2 0 0 0 2 2",key:"svg-0"}],["path",{d:"M17 4a2 2 0 0 1 2 2v3a2 3 0 0 0 2 3a2 3 0 0 0 -2 3v3a2 2 0 0 1 -2 2",key:"svg-1"}]],c=a("outline","braces","Braces",e);export{c as I};
